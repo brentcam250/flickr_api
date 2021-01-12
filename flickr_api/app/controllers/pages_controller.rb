622a7c0ed280c506f8a549896762afad118802b4
Brent Cameron <bjcameron@gmail.com>
2021-01-12 12:30:18 -0700

    tested api key, response from flickr, now need to parse responses into a nice format

--- flickr_api/app/controllers/pages_controller.rb
+++ flickr_api/app/controllers/pages_controller.rb
@@ -1,9 +1,23 @@
 class PagesController < ApplicationController
     def show
-        render template: "pages/#{params[:page]}"
+        @flickr = Flickr.new(ENV["flickr_key"], ENV["flickr_secret"])
+        @list = @flickr.photos.getRecent
+
+        if valid_page?
+            render template: "pages/#{params[:page]}"            
+        else
+            render file: "public/404.html", status: :not_found
+        end
     end
 
-    def base
-        @flickr = Flickr.new(ENV["flickr_key"], ENV["flickr_secret"])
+    # def home
+    #     @flickr = Flickr.new(ENV["flickr_key"], ENV["flickr_secret"])
+    # end
+
+    private 
+    def valid_page?
+        File.exists?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
     end
+
+
 end
