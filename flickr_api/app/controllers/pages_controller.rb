68d50fbc02829255f476b37f1fbe6b7590eea4b5
Brent Cameron <bjcameron@gmail.com>
2021-01-13 11:15:37 -0700

    able to get just a single users photos through flickr api by adjusting how parameters were passed to the api request

--- flickr_api/app/controllers/pages_controller.rb
+++ flickr_api/app/controllers/pages_controller.rb
@@ -1,14 +1,22 @@
 class PagesController < ApplicationController
     def show
         @flickr = Flickr.new(ENV["flickr_key"], ENV["flickr_secret"])
-
+        @id = 0
+        # @id = params[:flickr_id].to_s
+        # unless @id.nil?
         if params[:flickr_id]
             @id = params[:flickr_id].to_s
-            @list = @flickr.people.getPhotos(@id)
+            # test_id: { id: "75571860@N06" }
+            @params = {user_id: params[:flickr_id]}
+            @list = @flickr.people.getPhotos(@params)
+
+            # @list = nil# @flickr.people.getPhotos(@id)
+
         else
             @list = @flickr.photos.getRecent
         end
         
+
         if valid_page?
             render template: "pages/#{params[:page]}"            
         else
@@ -24,7 +32,8 @@ class PagesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def photo_params
         # params.fetch(:flight, {})
-        params.permit(:flickr_id, :page)
+        # params.require(:photos).permit(:flickr_id, :page)
+        params.require(:id).permit(:flickr_id)
     end
 
 
